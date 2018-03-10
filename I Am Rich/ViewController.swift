6a13659c4f709066b8f7be2539db88b2bbb6e5b3
Roshan Lakmal <roshanrox911@gmail.com>
2018-03-11 00:51:25 +0530

    Created the I am rich app

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Roshan Lakmal Elvitigala on 3/10/18.
+//  Copyright © 2018 Roshan Lakmal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
